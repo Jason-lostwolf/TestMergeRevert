fe2d94dfb9dfc4bd0a75c7eca926199f8239588a
Jason Yuan <jason_yuan@epam.com>
2020-04-01 22:07:08 +0800

    update branch

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Master";
+            this.label1.Text = "Branch";
             // 
             // Form1
             // 
